8404b4bb98b9ab23c128d76f6b6f76984afd438c
sethbehar <sethbehar@gmail.com>
2025-02-14 12:13:18 -0500

    server

--- server/server.py
+++ server/server.py
@@ -1,8 +1,9 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
 from huggingface_hub import InferenceClient
 from dotenv import load_dotenv
 import os
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
 
 # Load environment variables
 load_dotenv()
@@ -12,21 +13,26 @@ CORS(app)  # Allows frontend to call this API
 
 client = InferenceClient(api_key=os.getenv("MOSS_KEY"))
 
+
+
+
+
+
+
 @app.route("/chat", methods=["POST"])
 def chat():
     data = request.get_json()
     user_message = data.get("message", "")
-
     messages = [
         {"role": "user", "content": user_message}
     ]
 
     try:
         completion = client.chat.completions.create(
-        model="meta-llama/Meta-Llama-3-8B-Instruct", 
-        messages=messages, 
-        max_tokens=500
-    )
+            model="meta-llama/Meta-Llama-3-8B-Instruct", 
+            messages=messages, 
+            max_tokens=500
+        )
         response = completion.choices[0].message["content"]
         return jsonify({"response": response})
     except Exception as e:
